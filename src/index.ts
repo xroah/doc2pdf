06d3685df2a3d477c13091107c5e7aee06ea31eb
xroah <396069583@qq.com>
2020-12-10 22:19:33 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -8,43 +8,55 @@ import {spawn} from "child_process"
 
 const app = express()
 const pdfDir = path.join(__dirname, "../pdf")
+const docDir = path.join("__dirname", "../docs")
 
-app.use("/pdf", express.static(pdfDir))
-app.use("/convert2pdf", (req, res) => {
-    const url = req.query.url
-
-    if (!url) {
-        res.status(400)
-        res.end("error")
-
-        return
+function mkdir(dir: string) {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, {recursive: true})
     }
+}
 
-    downloadFile(url as string)
-    .then(filename => convert2pdf(filename as string))
-    .then(pdfFile => {
-        fs.createReadStream(pdfFile as string).pipe(res)
-    })
-})
+mkdir(pdfDir)
+mkdir(docDir)
 
 function downloadFile(url: string) {
     return new Promise((resolve, reject) => {
         const urlObj = new URL(url)
-        const {base} = path.parse(urlObj.pathname)
-        const filename = path.join(__dirname, "../docs", base)
-        const writeStream = fs.createWriteStream(filename);
+        const todayPath = getToday()
+        const {
+            base,
+            name
+        } = path.parse(urlObj.pathname)
+        const dir = path.join(docDir, todayPath)
+        const filename = path.join(dir, base)
+        const existingPdf = path.join(pdfDir, todayPath, `${name}.pdf`)
         let req = null
         const reqOpts = {
             hostname: urlObj.hostname,
-                method: "GET",
-                port: urlObj.port,
-                path: urlObj.pathname
+            method: "GET",
+            port: urlObj.port,
+            path: urlObj.pathname
         }
+
+        if (fs.existsSync(existingPdf)) {
+            resolve({
+                filename: existingPdf,
+                exists: true
+            })
+            return
+        }
+
+        mkdir(dir)
+
+        const writeStream = fs.createWriteStream(filename);
         const handleRes = (res: http.IncomingMessage) => {
             res.pipe(writeStream)
             res.on("end", () => {
                 writeStream.close()
-                resolve(filename)
+                resolve({
+                    filename,
+                    exists: false
+                })
             })
         }
 
@@ -57,32 +69,81 @@ function downloadFile(url: string) {
         if (req) {
             req.on("error", err => reject(err))
             req.end()
+        } else {
+            reject()
         }
     })
 }
 
 function convert2pdf(filename: string) {
-    return new Promise((resolve, reject) => {
-        spawn("libreoffice", [
-            "--headless",
-            "--convert-to",
-            "pdf",
-            "--outdir",
-            pdfDir,
-            filename
-        ])
-        .on("close", code => {
-            if (code === 0) {
-                const {name} = path.parse(filename)
+    const dir = path.join(pdfDir, getToday())
 
-                resolve(path.join(pdfDir, `${name}.pdf`))
-            } else {
-                reject()
-            }
-        })
-        .on("error", err => reject(err))
+    mkdir(dir)
+
+    return new Promise((resolve, reject) => {
+        spawn(
+            "libreoffice",
+            [
+                "--headless",
+                "--convert-to",
+                "pdf",
+                "--outdir",
+                dir,
+                filename
+            ]
+        )
+            .on("close", code => {
+                if (code === 0) {
+                    const {name} = path.parse(filename)
+
+                    resolve(path.join(dir, `${name}.pdf`))
+                    fs.unlink(filename, _ => { })
+                } else {
+                    reject()
+                }
+            })
+            .on("error", err => reject(err))
     })
 }
 
+app.use("/pdf", express.static(pdfDir))
+app.use("/convert2pdf", (req, res) => {
+    const url = req.query.url
+    const response = (pdfFile: string) => {
+        res.set("Content-Type", "application/pdf")
+        fs.createReadStream(pdfFile).pipe(res)
+    }
+
+    if (!url) {
+        res.status(400)
+        res.end("error")
+
+        return
+    }
+
+    downloadFile(url as string)
+        .then((obj: any) => {
+            if (!obj.exists) {
+                return convert2pdf(obj.filename)
+            }
+
+            response(obj.filename)
+
+            return false
+        })
+        .then(pdfFile => {
+            if (pdfFile !== false) {
+                response(pdfFile as string)
+            }
+        })
+        .catch(() => res.end("error"))
+})
+
+function getToday() {
+    const two = (num: number) => (100 + num).toString().substring(1)
+    const date = new Date()
+
+    return `${date.getFullYear()}/${two(date.getMonth() + 1)}/${two(date.getDate())}`
+}
 
 app.listen(8000)
